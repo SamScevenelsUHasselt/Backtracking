d0fe84d469ce5bc614fdaca7481a45f29a2044b4
Sam_Scevenels <Sam.Scevenels@student.UHasselt.be>
2023-04-19 13:35:43 +0200

    commented some fun configs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,21 @@ import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        int ploegen = 8;
-        int spelletjes = 4;
+        /* working configs:
+            -6 3 0 3 -- no backtracking needed
+            -4 4 0 4 -- a lot of backtracking needed
+            -6 3 0 2 -- geen oplossing -> voegt een ronde toe
+            -4 2 0 4 -- zeer kort met backtracking
+            -4 2 0 4 -- 2x rondes verhogen
+            -4 2 1 2 -- dubbels
+            -6 3 1 3 -- buddels zorgt voor backtracking
+        */
+
+
+        int ploegen = 4;
+        int spelletjes = 2;
         int dubbels = 0;
-        int rondes = 4;
+        int rondes = 2;
 
         Optional<String[][]> oplossing = Spelverdeling.spelverdeling(ploegen, spelletjes, dubbels, rondes);
         if(oplossing.isPresent()){
